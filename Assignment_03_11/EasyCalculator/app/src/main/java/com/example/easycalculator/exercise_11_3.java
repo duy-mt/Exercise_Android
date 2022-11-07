ea4a88193033b51d509ca1e4bd62796fbf39403a
duy.maitran <duy.mtdevhust@gmail.com>
2022-11-07 17:08:38 +0700

    'assignment-03/11'

---
+++ Assignment_03_11/EasyCalculator/app/src/main/java/com/example/easycalculator/exercise_11_3.java
@@ -0,0 +1,14 @@
+package com.example.easycalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class exercise_11_3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exercise113);
+    }
+}
